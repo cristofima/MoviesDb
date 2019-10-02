e9ea674ef50dd64ae4ac56f6afda0ee12d1b0f8e
Cristopher Coronado Moreira <cristofima@hotmail.com>
2019-10-01 21:59:44 -0500

    Nuevos servicios para las películas

--- src/app/shared/movies.service.ts
+++ src/app/shared/movies.service.ts
@@ -9,7 +9,7 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
-  private getQuery(query: string,params:string = '') {
+  private getQuery(query: string, params: string = '') {
     const url = `https://api.themoviedb.org/3/${query}`;
 
     const apiKey = '00d7fb865750066f0bee922febb0d108';
@@ -19,15 +19,27 @@ export class MoviesService {
     return this.http.get(`${url}${params}`);
   }
 
-  getMovies(pageNumber = 1){
-    if(pageNumber < 1){
+  getMovies(pageNumber = 1) {
+    if (pageNumber < 1) {
       pageNumber = 1;
-    }else if(pageNumber > 500){
+    } else if (pageNumber > 500) {
       pageNumber = 500;
     }
 
-    return this.getQuery('discover/movie',`page=${pageNumber}`).pipe(
+    return this.getQuery('discover/movie', `page=${pageNumber}`).pipe(
       map(data => data['results'])
     );
+  }
+
+  getMovieDetails(movieId: number) {
+    return this.getQuery(`movie/${movieId}`);
+  }
+
+  getMovieVideos(movieId: number) {
+    return this.getQuery(`movie/${movieId}/videos`);
+  }
+
+  getSimilarMovies(movieId: number) {
+    return this.getQuery(`movie/${movieId}/similar`);
   }
 }
