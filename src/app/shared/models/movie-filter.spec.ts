39351d60b52312277b175405d8c5a7b936ca3c57
Cristopher Coronado Moreira <cristofima@hotmail.com>
2024-02-01 12:14:51 -0500

    Delete spec.ts files

--- src/app/shared/models/movie-filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MovieFilter } from './movie-filter';
-
-describe('MovieFilter', () => {
-  it('should create an instance', () => {
-    expect(new MovieFilter()).toBeTruthy();
-  });
-});
--- src/app/shared/pipes/border-class.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BorderClassPipe } from './border-class.pipe';
-
-describe('BorderClassPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BorderClassPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
