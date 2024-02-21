b227a06c634769558ad313446574e75bd982909d
Cristopher Coronado <cristofima@hotmail.com>
2024-02-21 08:53:38 -0500

    Migrate to Bootstrap 5

--- src/app/features/movie/components/collection/collection.component.ts
+++ src/app/features/movie/components/collection/collection.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-collection-details',
-  templateUrl: './collection.component.html'
+  templateUrl: './collection.component.html',
+  styleUrls: ['./collection.component.scss']
 })
 export class CollectionDetailsComponent {
 
