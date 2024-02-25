85825b2361ff684f90aa3d7f99da33b734749278
Cristopher Coronado <cristofima@hotmail.com>
2024-02-25 10:26:30 -0500

    Improvements in media poster component

--- src/app/shared/components/media-poster/media-poster.component.ts
+++ src/app/shared/components/media-poster/media-poster.component.ts
@@ -10,16 +10,22 @@ import { TV } from 'src/app/core/models/tv.model';
 export class MediaPosterComponent implements OnInit {
 
   @Input({ required: true }) media: Movie | TV;
-  @Input() showSmallFormat = false;
+  @Input() sizeformat: 'small' | 'medium' | 'large' = 'large';
 
   posterSize = 'w342';
   fontSize = '1em';
   dateKey = 'releaseDate';
+  primaryColSize = 9;
 
   ngOnInit(): void {
-    if(this.showSmallFormat){
+    if(this.sizeformat === 'small'){
       this.posterSize = 'w154';
       this.fontSize = '0.9em';
+      this.primaryColSize = 12;
+    }else if(this.sizeformat === 'medium'){
+      this.posterSize = 'w185';
+      this.fontSize = '0.93em';
+      this.primaryColSize = 12;
     }
 
     if('firstAirDate' in this.media){
