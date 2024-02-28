c0e7b7a5e36f93d29d687e0af0af18d5cf2485de
Cristopher Coronado <cristofima@hotmail.com>
2024-02-28 10:52:48 -0500

    Fix circle-progress position in media-poster component

--- src/app/shared/components/media-poster/media-poster.component.ts
+++ src/app/shared/components/media-poster/media-poster.component.ts
@@ -16,18 +16,21 @@ export class MediaPosterComponent implements OnInit {
   fontSize = '1em';
   dateKey = 'releaseDate';
   titleFontSize = 15;
-  bottomPosition = '85px';
+  bottomPosition = '-15px';
+  minImgHeight = '223px';
 
   ngOnInit(): void {
     if(this.sizeformat === 'small'){
       this.posterSize = 'w154';
       this.fontSize = '0.9em';
       this.titleFontSize = 13;
-      this.bottomPosition = '130px';
+      this.bottomPosition = '-20px';
     }else if(this.sizeformat === 'medium'){
       this.posterSize = 'w185';
       this.fontSize = '0.93em';
-      this.bottomPosition = '82px';
+      this.bottomPosition = '-20px';
+    }else if(this.sizeformat === 'large'){
+      this.minImgHeight = '457px';
     }
 
     if('firstAirDate' in this.media){
