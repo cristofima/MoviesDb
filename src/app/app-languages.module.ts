370fa614a9193633c8c96552f2acefbf0a1965eb
Cristopher Coronado <cristofima@hotmail.com>
2024-03-08 12:11:03 -0500

    Move ngx-translate and locale imports to AppLanguagesModule

---
+++ src/app/app-languages.module.ts
@@ -0,0 +1,53 @@
+import { LOCALE_ID, ModuleWithProviders, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import localeEs from '@angular/common/locales/es';
+import localePt from '@angular/common/locales/pt';
+import localeFr from '@angular/common/locales/fr';
+import localeDe from '@angular/common/locales/de';
+import localeIt from '@angular/common/locales/it';
+import localeRu from '@angular/common/locales/ru';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { SettingsService } from "./core/services/settings.service";
+
+registerLocaleData(localeEs, 'es-MX');
+registerLocaleData(localePt, 'pt-PT');
+registerLocaleData(localeFr, 'fr-FR');
+registerLocaleData(localeDe, 'de-DE');
+registerLocaleData(localeIt, 'it-IT');
+registerLocaleData(localeRu, 'ru-RU');
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      defaultLanguage: 'en-US',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+  ],
+  exports: [TranslateModule]
+})
+export class AppLanguagesModule {
+  static forRoot(): ModuleWithProviders<AppLanguagesModule> {
+    return {
+      ngModule: AppLanguagesModule,
+      providers: [
+        {
+          provide: LOCALE_ID,
+          deps: [SettingsService],
+          useFactory: (settingsService) => settingsService.getLanguage()
+        }
+      ]
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -15,32 +15,11 @@ import { PageNotFoundComponent } from "./shared/components/error-pages/page-not-
 import { SharedModule } from "./shared/shared.module";
 
 import { NgCircleProgressModule } from "ng-circle-progress";
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import { SettingsService } from "./core/services/settings.service";
-
-import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es';
-import localePt from '@angular/common/locales/pt';
-import localeFr from '@angular/common/locales/fr';
-import localeDe from '@angular/common/locales/de';
-import localeIt from '@angular/common/locales/it';
-import localeRu from '@angular/common/locales/ru';
+import { AppLanguagesModule } from "./app-languages.module";
 
 const ngHttpCachingConfig: NgHttpCachingConfig = {
   store: new NgHttpCachingLocalStorage()
 };
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
-registerLocaleData(localeEs, 'es-MX');
-registerLocaleData(localePt, 'pt-PT');
-registerLocaleData(localeFr, 'fr-FR');
-registerLocaleData(localeDe, 'de-DE');
-registerLocaleData(localeIt, 'it-IT');
-registerLocaleData(localeRu, 'ru-RU');
 
 @NgModule({
   declarations: [
@@ -53,14 +32,6 @@ registerLocaleData(localeRu, 'ru-RU');
     AppRoutingModule,
     HttpClientModule,
     NgHttpCachingModule.forRoot(ngHttpCachingConfig),
-    TranslateModule.forRoot({
-      defaultLanguage: 'en-US',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
     NgCircleProgressModule.forRoot({
       outerStrokeWidth: 12,
       titleColor: "white",
@@ -74,6 +45,7 @@ registerLocaleData(localeRu, 'ru-RU');
       showZeroOuterStroke: false,
       showInnerStroke: false
     }),
+    AppLanguagesModule.forRoot(),
     SharedModule
   ],
   providers: [
@@ -81,11 +53,6 @@ registerLocaleData(localeRu, 'ru-RU');
       provide: HTTP_INTERCEPTORS,
       useClass: GlobalHttpInterceptor,
       multi: true,
-    },
-    {
-      provide: LOCALE_ID,
-      deps: [SettingsService],
-      useFactory: (settingsService) => settingsService.getLanguage()
     }
   ],
   bootstrap: [AppComponent],
